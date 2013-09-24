aaf039ffad5958bc0015425497684d8967e5b146
Phil Schleihauf <uniphil@gmail.com>
2013-09-24 10:39:17 -0400

    add template rendering config and fix a tiny bug in base.html

--- wirey.py
+++ wirey.py
@@ -12,7 +12,10 @@
 from flask import Flask, url_for, render_template, Markup
 from werkzeug.routing import BuildError
 
+
 app = Flask(__name__)
+app.jinja_env.line_statement_prefix = '%%'
+app.jinja_env.line_comment_prefix = '##'
 
 
 def monkey_url_for(endpoint, **kwargs):
@@ -27,13 +30,17 @@ fake_gettext = lambda message: Markup(message)  # haxx me bro
 
 @app.route('/')
 def home():
-    return render_template('home.html', url_for=monkey_url_for, _=fake_gettext)
+    return render_template('home.html', url_for=monkey_url_for, _=lambda blah: blah)
 
 
 @app.route('/<page>')
 def page(page):
     return render_template('{}.html'.format(page), url_for=monkey_url_for, _=fake_gettext)
 
+
+@app.route('/favicon.ico')
+def no_favicon_error():
+    return ''
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
