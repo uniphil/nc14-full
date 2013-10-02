6d380e551e6513370cd8ee526bd5f9156b8de6df
Riley Shaw <rileyjshaw@gmail.com>
2013-10-02 19:55:24 -0400

    Start work on sticky header

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,3 +3,14 @@ $(function() {
     $('#email').focus();
   });
 });
+
+function checkScroll(event){
+  console.log($(window).scrollTop(), lockPoint)
+  if($(window).scrollTop() >= lockPoint){
+    $lockBar.addClass("hlock");
+    $main.addClass("mlock");
+  }else{
+    $lockBar.removeClass('hlock');
+    $main.removeClass('mlock');
+  }
+}
