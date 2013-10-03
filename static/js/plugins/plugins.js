a9840ae83d751fadcc7efca7b6e0ce6b5e0fac44
Philip Schleihauf <uniphil@gmail.com>
2013-10-03 02:15:31 -0400

    buddy selector

--- static/js/plugins/plugins.js
+++ static/js/plugins/plugins.js
@@ -1,24 +1,24 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
 
-    while (length--) {
-        method = methods[length];
+  while (length--) {
+    method = methods[length];
 
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
     }
+  }
 }());
 
 /////////////////////////////////////
@@ -27,49 +27,49 @@
 // from http://css-tricks.com/snippets/jquery/smooth-scrolling/
 
 function filterPath(string) {
-return string
-.replace(/^\//,'')
-.replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-.replace(/\/$/,'');
+  return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
 }
 var locationPath = filterPath(location.pathname);
 var scrollElem = scrollableElement('html', 'body');
 
 $('a[href*=#]').each(function() {
-var thisPath = filterPath(this.pathname) || locationPath;
-if (locationPath == thisPath &&
-    (location.hostname == this.hostname || !this.hostname) &&
-    this.hash.replace(/#/,'') ) {
-  var $target = $(this.hash), target = this.hash;
-  if (target) {
-    var targetOffset = $target.offset().top;
-    $(this).click(function(event) {
-      event.preventDefault();
-      $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-        location.hash = target;
+  var thisPath = filterPath(this.pathname) || locationPath;
+  if (locationPath == thisPath &&
+      (location.hostname == this.hostname || !this.hostname) &&
+      this.hash.replace(/#/,'') ) {
+    var $target = $(this.hash), target = this.hash;
+    if (target) {
+      var targetOffset = $target.offset().top;
+      $(this).click(function(event) {
+        event.preventDefault();
+        $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+          location.hash = target;
+        });
       });
-    });
+    }
   }
-}
 });
 
 // use the first element that is "scrollable"
 function scrollableElement(els) {
-for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-  var el = arguments[i],
-      $scrollElement = $(el);
-  if ($scrollElement.scrollTop()> 0) {
-    return el;
-  } else {
-    $scrollElement.scrollTop(1);
-    var isScrollable = $scrollElement.scrollTop()> 0;
-    $scrollElement.scrollTop(0);
-    if (isScrollable) {
+  for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+    var el = arguments[i],
+        $scrollElement = $(el);
+    if ($scrollElement.scrollTop()> 0) {
       return el;
+    } else {
+      $scrollElement.scrollTop(1);
+      var isScrollable = $scrollElement.scrollTop()> 0;
+      $scrollElement.scrollTop(0);
+      if (isScrollable) {
+        return el;
+      }
     }
   }
-}
-return [];
+  return [];
 }
 
 
@@ -78,12 +78,28 @@ return [];
 ////////////////////
 (function() {
   var Switcher = function(lep_box) {
-    this.width = lep_box.width();
-    this.boxes = $("#learn,#innovate,#connect", lep_box);
-    this.boxes.width(this.width);
-    this.boxes.wrap('<div>').each(function() {
+    var width = lep_box.width()
+    $("#learn, #innovate, #connect", lep_box).wrap('<div>').each(function() {
       $this = $(this);
       $this.parent().attr("id", $this.attr("id") + "-wrap");
+    });
+    $(".about-lep-tabs a", lep_box).click(function() {
+      $(".about-lep-tabs a", lep_box).removeClass("selected");
+      $(this).addClass("selected");
+      switch($(this).attr("href")) {
+        case "#learn":
+          $("#learn-wrap").css({width: width});
+          $("#innovate-wrap").css({width: width});
+          break;
+        case "#innovate":
+          $("#learn-wrap").css({width: 0});
+          $("#innovate-wrap").css({width: width});
+          break;
+        case "#connect":
+          $("#learn-wrap").css({width: 0});
+          $("#innovate-wrap").css({width: 0});
+          break;
+      }
     });
   };
 
@@ -91,4 +107,4 @@ return [];
   if (switcher_el.length > 0) {
     new Switcher(switcher_el);
   }
-})()
+})();
